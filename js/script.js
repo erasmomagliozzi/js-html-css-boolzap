2bd6420948904083671ea297ab3279d956f87fa8
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-23 17:20:09 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,26 +1,14 @@
 $(document).ready(
   function(){
-   // var value = $('.navbar-text-message input:text').text('');
-   // console.log(value);
+
    $('.fa-telegram-plane').click(
      function() {
-       // var value = $( '.navbar-text-message input' );
-       // console.log(value);
-       // $('.mex-user.input_message').text(value);
-       // $( ".mex-user.input_message" ).val( value );
-       //
-       // $('.div').addClass('.mex-user');
-       $('p .input-message').val('.navbar-text-message input:text');
+      var testoMessaggio = $('.navbar-text-message input value');
+      console.log(testoMessaggio);
+
+      // $('.navbar-text-message input:text').val("GeeksForGeeks");
 
      }
    );
   }
 );
-
-
-//
-// $( "input" )
-//   .keyup(function() {
-//     var value = $( this ).val();
-//     $( "p" ).text( value );
-//   })
