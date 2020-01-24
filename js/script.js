38f624de6342c040190d9f6dd3bd0aec3d80f838
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-24 17:32:15 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 $(document).ready(
   function(){
 
-   $('.fa-telegram-plane').click(
+   $('.icon-send').click(
      function() {
-      var testoMessaggio = $('.navbar-text-message input value');
-      console.log(testoMessaggio);
+       inviaMessaggio();
+     });
 
-      // $('.navbar-text-message input:text').val("GeeksForGeeks");
+     // FUNZIONE INVIA MESSAGGIO
+ function inviaMessaggio(){
+   var textMessage = $('input.send-message').val();
 
-     }
-   );
+   if(textMessage.length != 0) {
+    var newMessage = $('.template .messaggio').clone();
+    console.log(newMessage);
+
+    newMessage.find('.message-text').text(textMessage);
+    var data = new Date();
+    var hours = addZero(data.getHours());
+    var minutes = addZero(data.getMinutes());
+    var time = hours +':'+ minutes;
+
+    newMessage.find('.message-time').text(time);
+    newMessage.addClass('utente');
+    $('.spazio-messaggi').append(newMessage);
+
+    $('input.send-message').val('');
+ }
+}
+// FUNZIONE AGGIUNGI ZERO AD ORARIO
+function addZero(number) {
+  if(number < 10) {
+    number = '0' + number;
   }
-);
+  return number;
+}
+
+  });
