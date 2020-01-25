047d465ffa45bfdf961b9fc19f2826500e30ac83
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-25 16:17:24 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ $(document).ready(
    $('.icon-send').click(
      function() {
        inviaMessaggio();
+       pcMessage();
      });
+   // $('.search').click(
+   //   function(){
+   //     searchContact(contatti);
+   //   }
+   // )
+     time();
+
 
      // FUNZIONE INVIA MESSAGGIO
  function inviaMessaggio(){
@@ -35,4 +43,47 @@ function addZero(number) {
   return number;
 }
 
+function pcMessage(){
+  var messaggioComputer = $('.template .messaggio').clone();
+  console.log(messaggioComputer);
+
+  messaggioComputer.find('.message-text').text('Ok');
+  messaggioComputer.addClass('computer');
+  $('.spazio-messaggi').append(messaggioComputer);
+
+  var data = new Date();
+  var hours = addZero(data.getHours());
+  var minutes = addZero(data.getMinutes());
+  var time = hours +':'+ minutes;
+
+  messaggioComputer.find('.message-time').text(time);
+}
+
+// function searchContact(arrayName){
+//
+//   var cerca = $('input.cerca').val();
+//   console.log(cerca);
+//
+//   for(var i = 0; i < arrayName.length; i++ ){
+//     if(arrayName.includes(cerca) == false){
+//       $('mex-contatto').addClass('none');
+//     }
+//   }
+// }
+
+function time(){
+  var data = new Date();
+  var hours = addZero(data.getHours());
+  var minutes = addZero(data.getMinutes());
+  var time = hours +':'+ minutes;
+
+  $('.mex-contatto').find('.contact_message-bottom small').html(time);
+}
+
   });
+// var contatti = [];
+//  for (var j = 0; j < 7; j++){
+//  var cont = $('.anteprima-mex h3').text();
+// contatti.push(cont);
+// }
+// console.log(contatti);
